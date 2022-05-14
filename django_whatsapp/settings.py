840832fa5e537adb7a6f846601d441f8c2819126
zahur <zahurmeerun@hotmail.com>
2022-05-14 16:12:51 +0400

    create: profile app to store images

--- django_whatsapp/settings.py
+++ django_whatsapp/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
     'home',
     'message',
+    'profile',
 ]
 
 MIDDLEWARE = [
--- django_whatsapp/urls.py
+++ django_whatsapp/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("", include("home.urls")),
     path("message/", include("message.urls")),
+    path("profile/", include("profile.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

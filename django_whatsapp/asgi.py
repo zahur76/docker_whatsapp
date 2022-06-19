4b1baab4bffab1b15b550fe5c1ba6c2c347fe6d4
zahur <zahurmeerun@hotmail.com>
2022-06-19 10:01:54 +0400

    add: logic to open and close webscoket connections

--- django_whatsapp/asgi.py
+++ django_whatsapp/asgi.py
@@ -1,14 +1,24 @@
-## tic_tac_toe/asgi.py
 import os
 
-import django
-from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_whatsapp.settings')
-django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_whatsapp.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+import message.routings
 
 application = ProtocolTypeRouter({
-  "http": AsgiHandler(),
-  ## IMPORTANT::Just HTTP for now. (We can add other protocols later.)
+  "http": django_asgi_app,
+  "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                message.routings.websocket_urlpatterns
+            )
+        )
+    ),
 })

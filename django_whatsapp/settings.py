63ef1c307af1a7b24094da96736deb266fed2f32
zahur <zahurmeerun@hotmail.com>
2022-06-20 10:24:58 +0400

    update: settings.oy for deployment heroku

--- django_whatsapp/settings.py
+++ django_whatsapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-# import dj_database_url
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'channels',
+    'storages',
 
     'home',
     'message',
@@ -131,19 +132,7 @@ else:
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'username_yourdbname',
-            'USER': 'username_yourdbusername',
-            'PASSWORD': 'youruserpassword',
-            'HOST': 'localhost',
-            'PORT': '3306',
-            'OPTIONS': {
-                'sql_mode': 'STRICT_ALL_TABLES',
-            },
-        }
-    }
+   DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 else:
     DATABASES = {
         'default': {
@@ -209,6 +198,34 @@ MEDIA_URL = "/media/"
 # media save location
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+if "USE_AWS" in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
+        "CacheControl": "max-age=94608000",
+    }
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = "django-whatsapp-zahur"
+    AWS_S3_REGION_NAME = "ap-southeast-1"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    # Static and media files
+    # To allow django-admin collectstatic to automatically put your static files in your bucket
+    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STATICFILES_LOCATION = (
+        "static"  # store files under directory `static/` in bucket `my-app-bucket`
+    )
+    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
+    MEDIAFILES_LOCATION = (
+        "media"  # store files under directory `media/` in bucket `my-app-bucket`
+    )
+
+    # Override static and media URLs in production
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -219,9 +236,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'molacuizine@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'mail.hansolo.digital'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
